8293670cd0082b55a2051fdfc14d0aff5f185143
Emanuel Celayes <ecelayes@gmail.com>
2023-03-09 01:10:44 -0300

    Rutas y archivos Views creados

---
+++ src/views/NosotrosView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Nosotros</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/PlanesView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Planes</h1>
+  </div>
+</template>
+
+<style></style>
