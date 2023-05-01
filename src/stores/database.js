e26adf564f63385a746a2eabb5bd204a12601f29
elbader17 <badereduardo@gmail.com>
2023-05-01 17:43:53 -0300

    little changes

--- src/stores/database.js
+++ src/stores/database.js
@@ -1,36 +1,53 @@
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, query, updateDoc } from 'firebase/firestore/lite';
-import { db, auth } from '@/firebaseConfig';
-import { defineStore } from 'pinia';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc
+} from 'firebase/firestore/lite'
+import { db, auth } from '@/firebaseConfig'
+import { defineStore } from 'pinia'
 
 export const useDatabaseStore = defineStore('databaseStore', {
   state: () => ({
     loadingDoc: false,
-    clients: [],
+    clients: []
   }),
   actions: {
     async getClients() {
       if (this.clients.length !== 0) {
         return
       }
-      this.loadingDoc = true;
+
+      this.loadingDoc = true
+
       try {
-        const q = query(collection(db, 'clients'));
-        const querySnapshot = await getDocs(q);
+        const q = query(collection(db, 'clients'))
+        const querySnapshot = await getDocs(q)
+
         querySnapshot.forEach((doc) => {
-          console.log(doc.id, doc.data());
-          this.clients.push({
+          console.log(doc.id, doc.data())
+
+          const client = {
             id: doc.id,
             ...doc.data()
-          })
+          }
+
+          this.clients.push(client)
         })
       } catch (error) {
-        console.log(error);
+        console.log(error)
       } finally {
-        this.loadingDoc = false;
+        this.loadingDoc = false
       }
     },
+
     async addClient(client) {
-      this.loadingDoc = true;
+      this.loadingDoc = true
+
       try {
         const docObj = {
           name: client.name,
@@ -42,82 +59,89 @@ export const useDatabaseStore = defineStore('databaseStore', {
           city: client.city,
           user: auth.currentUser.uid
         }
-        const docRef = await addDoc(collection(db, 'clients'), docObj);
-        console.log(docRef.id);
-        this.clients.push({
+
+        const docRef = await addDoc(collection(db, 'clients'), docObj)
+        console.log(docRef.id)
+
+        const newClient = {
           id: docRef.id,
           ...docObj
+        }
+
+        this.clients.push(newClient)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        this.loadingDoc = false
+      }
+    },
+
+    async readClient(id) {
+      this.loadingDoc = true
+
+      try {
+        const docRef = doc(db, 'clients', id)
+        const docSnap = await getDoc(docRef)
+
+        if (!docSnap.exists()) {
+          throw new Error('No existe el cliente')
+        }
+
+        if (docSnap.data().user !== auth.currentUser.uid) {
+          throw new Error('No tienes permiso')
+        }
+
+        const client = {
+          name: docSnap.data().name,
+          surname: docSnap.data().surname,
+          dni: docSnap.data().dni,
+          birthdate: docSnap.data().birthdate,
+          phone: docSnap.data().phone,
+          address: docSnap.data().address,
+          city: docSnap.data().city
+        }
+
+        return client
+      } catch (error) {
+        console.log(error.message)
+      } finally {
+        this.loadingDoc = false
+      }
+    },
+
+    async updateClient(id, client) {
+      this.loadingDoc = true
+
+      try {
+        const docRef = doc(db, 'clients', id)
+        await updateDoc(docRef, client)
+
+        this.clients = this.clients.map((item) => {
+          if (item.id === id) {
+            return {
+              ...item,
+              ...client
+            }
+          } else {
+            return item
+          }
         })
       } catch (error) {
-        console.log(error);
+        console.log(error)
       } finally {
-        this.loadingDoc = false;
+        this.loadingDoc = false
       }
     },
-    async readClient(id) {
-      this.loadingDoc = true;
-      try {
-        const docRef = doc(db, 'clients', id);
-        const docSnap = await getDoc(docRef);
-        if (!docSnap.exists()) {
-          throw new Error("No existe el cliente");
-        }
-        if (docSnap.data().user === auth.currentUser.uid) {
-          return {
-            name: docSnap.data().name,
-            surname: docSnap.data().surname,
-            dni: docSnap.data().dni,
-            birthdate: docSnap.data().birthdate,
-            phone: docSnap.data().phone,
-            address: docSnap.data().address,
-            city: docSnap.data().city
-          };
-        } else {
-          throw new Error("No tienes permiso");
-        }
-      } catch (error) {
-        console.log(error.message);
-      } finally {
-        this.loadingDoc = false;
-      }
-    },
-    async updateClient(id, client) {
-      this.loadingDoc = true;
-      try {
-        const docRef = doc(db, 'clients', id);
-        await updateDoc(docRef, {
-          name: client.name,
-          surname: client.surname,
-          dni: client.dni,
-          birthdate: client.birthdate,
-          phone: client.phone,
-          address: client.address,
-          city: client.city
-        });
-        this.clients = this.clients.map(item => item.id === id ? ({
-          ...item,
-          name: client.name,
-          surname: client.surname,
-          dni: client.dni,
-          birthdate: client.birthdate,
-          phone: client.phone,
-          address: client.address,
-          city: client.city
-        }) : item)
-      } catch (error) {
-        console.log(error);
-      } finally {
-        this.loadingDoc = false;
-      }
-    },
+
     async deleteClient(id) {
       try {
-        const docRef = doc(db, 'clients', id);
-        await deleteDoc(docRef);
-        this.clients = this.clients.filter(item => item.id !== id)
+        const docRef = doc(db, 'clients', id)
+        await deleteDoc(docRef)
+
+        this.clients = this.clients.filter((item) => item.id !== id)
       } catch (error) {
-        console.log(error);
-      }/*  finally {
+        console.log(error)
+      } /*  finally {
 
       } */
     }
