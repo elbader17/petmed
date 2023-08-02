761df89cd9222ac09a7d0fb5a19952e1c7930d97
elbader17 <badereduardo@gmail.com>
2023-08-02 05:16:43 -0300

    added credentil card

--- src/components/DashboardPetsEdit.vue
+++ src/components/DashboardPetsEdit.vue
@@ -1,17 +1,34 @@
 <script setup>
-import { useDatabasePetStore } from '@/stores/databasePet';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
+import { useUserStore } from '@/stores/user'
 
-const props = defineProps(['item', 'plans']);
+const userStore = useUserStore()
+const code = ref()
+const showCard = ref(true)
+const textButton = ref('Generar Token')
 
-const databasePetStore = useDatabasePetStore();
+const generate = async () => {
+  try {
+    userStore.createCode(pet.value.numAffiliate)
+    code.value = userStore.userCode
+    showCard.value = !showCard.value
+    if(textButton.value === 'Generar Token') {
+      textButton.value = 'Regresar'
+    } else {
+      textButton.value = 'Generar Token'
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+const props = defineProps(['item', 'plans'])
 
-const options = ref([]);
+const options = ref([])
 
 onMounted(async () => {
-  options.value = props.plans;
-  pet.value = props.item;
-});
+  options.value = props.plans
+  pet.value = props.item
+})
 
 const pet = ref({
   client: '',
@@ -23,78 +40,225 @@ const pet = ref({
   color: '',
   plan: '',
   numAffiliate: '',
-});
-
-const handleSubmit = () => {
-  databasePetStore.updatePet(props.item.id, pet.value);
-}
+  render_not: false
+})
 </script>
 
 <template>
-  <section>
-    <form class="form" @submit.prevent="handleSubmit">
-      <label class="form-title" for="edit-name">Nombre:</label>
-      <input class="form-input" type="text" id="edit-name" name="edit-name" v-model="pet.name">
+  <div class="dashboard">
+    <section v-if="showCard" class="credit-card">
+      <div class="card-info">
+        <div class="card-header">
+          <h1>Credencial de Servicio</h1>
+          <img
+            src="https://s3.ppllstatics.com/laverdad/www/multimedia/202211/23/media/cortadas/como-saber-cuanto-frio-soportar-perro-kpaB-U180813416069owF-1248x770@La%20Verdad.jpg"
+            alt="Pet Photo"
+            class="pet-photo"
+          />
+        </div>
 
-      <label class="form-title" for="edit-birthday">Fecha de nacimiento:</label>
-      <input class="form-input" type="date" id="edit-birthday" name="edit-birthday" v-model="pet.birthdate">
+        <div class="card-field">
+          <span class="field-title">Nombre:</span>
+          <div class="field-value">{{ pet.name }}</div>
+        </div>
 
-      <label class="form-title" for="edit-animal">Tipo:</label>
-      <input class="form-input" type="text" id="edit-animal" name="edit-animal" v-model="pet.animal">
+        <div class="card-field">
+          <span class="field-title">Fecha de nacimiento:</span>
+          <div class="field-value">{{ pet.birthdate }}</div>
+        </div>
 
-      <label class="form-title" for="edit-breed">Raza:</label>
-      <input class="form-input" type="text" id="edit-breed" name="edit-breed" v-model="pet.breed">
+        <div class="card-field">
+          <span class="field-title">Tipo:</span>
+          <div class="field-value">{{ pet.animal }}</div>
+        </div>
 
-      <label class="form-title" for="edit-sex">Sexo:</label>
-      <select class="form-input" id="edit-sex" name="edit-sex" v-model="pet.sex">
-        <option value="Macho">Macho</option>
-        <option value="Hembra">Hembra</option>
-      </select>
+        <div class="card-field">
+          <span class="field-title">Raza:</span>
+          <div class="field-value">{{ pet.breed }}</div>
+        </div>
 
-      <label class="form-title" for="edit-color">Color:</label>
-      <input class="form-input" type="text" id="edit-color" name="edit-color" v-model="pet.color">
+        <div class="card-field">
+          <span class="field-title">Sexo:</span>
+          <div class="field-value">{{ pet.sex }}</div>
+        </div>
 
-      <button class="form-button" type="submit">Editar</button>
-    </form>
-  </section>
+        <div class="card-field">
+          <span class="field-title">Color:</span>
+          <div class="field-value">{{ pet.color }}</div>
+        </div>
+
+        <div class="logo-container">
+          <img src="../../src/assets/logo.svg" alt="Logo" class="logo" />
+        </div>
+      </div>
+    </section>
+    <div class="code-container" v-if="!showCard">
+      <p>Este token es unico, es valido solo por 5 minutos</p>
+      <p>En el caso de que expire el token, debe generar uno nuevo</p>
+      <p class="code-text">{{ code }}</p>
+    </div>
+
+    <!-- boton para generar token -->
+    <button class="form-button" @click="generate">{{ textButton }}</button>
+  </div>
 </template>
 
 <style scoped>
-.form {
+.credit-card {
   display: flex;
   flex-direction: column;
-  padding: 0.25rem;
+  padding: 1rem;
+  width: 780px; /* Adjust the width as needed */
+  background-color: #a188fc; /* Add a background color for the card */
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
-
-.form-title {
-  font-size: 1.15rem;
-  line-height: 1.75rem;
-  font-weight: 400;
-  margin-top: 0.5rem;
-}
-
-.form-input {
-  padding: 0.5rem;
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-  width: 100%;
-  border-radius: 0.5rem;
-  border: 1px solid #333333;
-}
-
 .form-button {
   padding: 0.5rem 1rem;
-  margin: 1.25rem;
+  margin-left: 4.5rem;
+  margin-top: 1rem;
+  width: 80%;
   border: none;
   border-radius: 1.25rem;
   font-size: 1rem;
   color: #fff;
-  background-color: #8D57B0;
+  background-color: #8d57b0;
   box-shadow: 0 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
   cursor: pointer;
 }
+.card-info {
+  display: flex;
+  flex-direction: column;
+}
 
-.form-button:hover {
-  background-color: #794899;
+.card-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+
+.pet-photo {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 1rem;
+}
+
+h1 {
+  font-size: 2.4rem; /* Increased font size */
+  font-weight: bold;
+  color: #fff;
+  margin: 0;
+  padding: 0.5rem 1rem; /* Added padding to create a background */
+  background-color: #64b5f6; /* Background color for the h1 */
+  border-radius: 5px; /* Rounded corners */
+}
+
+.data-container {
+  display: flex;
+  flex-direction: column;
+}
+.code-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Center the code text horizontally */
+  margin-top: 1rem; /* Add some top margin for spacing */
+  text-align: center; /* Center the text inside the container */
+  color: #8d57b0; /* Set the text color to purple */
+  height: 15rem;
+}
+
+.code-text {
+  font-size: 2.4rem; /* Increased font size */
+  font-weight: bold;
+  margin-bottom: 1rem; /* Add some bottom margin for spacing */
+}
+
+.card-field {
+  display: flex;
+  margin-bottom: 0.25rem; /* Reduced space between fields */
+}
+
+.field-title {
+  font-size: 1.2rem; /* Increased font size */
+  font-weight: bold;
+  width: 150px;
+  color: #fff;
+}
+
+.field-value {
+  flex: 1;
+  font-size: 1.2rem; /* Increased font size */
+  font-weight: bold; /* Added font weight */
+  padding: 0.25rem;
+  color: #fff; /* Text color in white */
+  width: 50%; /* Occupy 50% of the width */
+}
+
+.logo-container {
+  position: absolute; /* Set the logo container to absolute positioning */
+  right: 3.2rem; /* Distance from the right side */
+  bottom: 4rem; /* Distance from the bottom */
+}
+
+.logo {
+  width: 150px; /* Adjust the size as needed */
+  filter: brightness(0) invert(1); /* Make the logo white */
+}
+
+@media (max-width: 600px) {
+  .dashboard {
+    transform: rotate(90deg);
+    margin-bottom: 1rem ;
+  }
+  .credit-card {
+    width: 600px;
+    height: 180%;
+    margin: 10px 10px ;  
+    padding: 0px;
+  }
+  .logo{
+    width: 100px;
+  }
+  .logo-container{
+    right: -15rem;
+    bottom: 4rem;
+  }
+  .field-title{
+    width: 100px;
+    font-size: 0.8rem;
+    margin-left: 0.7rem;
+  }
+  .field-value{
+    width: 100px;
+    font-size: 0.8rem;
+  }
+  .card-field{
+    margin-bottom: 0.1rem;
+  }
+  .card-info{
+    margin-bottom: 0.1rem;
+    height: 10%;
+  }
+  .card-header{
+    margin-bottom: 0.1rem;
+  }
+  .pet-photo{
+    width: 100px;
+    height: 100px;
+    margin-right: 0.5rem;
+    margin-top: 0.5rem;
+  }
+  h1{
+    font-size: 1.5rem;
+  }
+  .form-button{
+    margin-left: 10rem;
+    margin-top: 0.5rem;
+    width: 80%;
+    font-size: 0.8rem;
+  }
 }
 </style>
