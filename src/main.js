4b04973a1644741bdead9c4a972657a5de59652b
Emanuel Celayes <ecelayes@gmail.com>
2024-01-16 00:36:42 -0300

    Refactor font-awesome import and component registration

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue';
 import router from './router';
 import { VueReCaptcha } from 'vue-recaptcha-v3';
-import { FontAwesomeIcon } from './fontAwesome';
+
+let FontAwesomeIcon;
+
+const loadFontAwesomeIcon = async () => {
+  const module = await import('./fontAwesome');
+  FontAwesomeIcon = module.FontAwesomeIcon;
+};
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -11,5 +17,8 @@ const pinia = createPinia();
 app.use(router);
 app.use(pinia);
 app.use(VueReCaptcha, { siteKey: import.meta.env.VITE_APP_RECAPTCHA_KEY });
-app.component('font-awesome-icon', FontAwesomeIcon);
-app.mount('#app');
+
+loadFontAwesomeIcon().then(() => {
+  app.component('font-awesome-icon', FontAwesomeIcon);
+  app.mount('#app');
+});
