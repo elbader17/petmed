833f533b16d16ec667b7490b0f70bb595422ac12
Emanuel Celayes <ecelayes@gmail.com>
2024-01-15 23:59:00 -0300

    Add vue-recaptcha-v3 package and update ContactForm.vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia'
 import App from './App.vue';
 import router from './router';
+import { VueReCaptcha } from 'vue-recaptcha-v3';
 import { FontAwesomeIcon } from './fontAwesome';
 
 const app = createApp(App);
@@ -9,5 +10,6 @@ const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
+app.use(VueReCaptcha, { siteKey: import.meta.env.VITE_APP_RECAPTCHA_KEY });
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
