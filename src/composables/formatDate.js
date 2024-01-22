b4c00e11973cef729d1cfe85b84b5d12c5da202f
Emanuel Celayes <ecelayes@gmail.com>
2024-01-22 14:59:18 -0300

    Fix date formatting issue in useFormatDate

--- src/composables/formatDate.js
+++ src/composables/formatDate.js
@@ -6,8 +6,16 @@ export const useFormatDate = () => {
 
     const separator = date.includes('/') ? '/' : '-';
     const dateParts = date.split(separator);
-    let day = dateParts[separator === '/' ? 0 : 2];
-    let month = dateParts[separator === '/' ? 0 : 2];
+    let day, month;
+
+    if (separator === '/') {
+      month = dateParts[0];
+      day = dateParts[1];
+    } else {
+      day = dateParts[0];
+      month = dateParts[1];
+    }
+
     const year = dateParts[2];
 
     day = day.length === 1 ? '0' + day : day;
